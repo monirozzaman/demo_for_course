6d66c4e48b5102d226f926ec853573225158d8c2
unknown <moniruzzaman35-223@diu.edu.bd>
2020-01-28 19:49:15 +0600

    first commit

---
+++ src/main/java/com/spring/studentportal/model/SignUpModel.java
@@ -0,0 +1,59 @@
+package com.spring.studentportal.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "students")
+public class SignUpModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String dept;
+
+    private String studentId;
+
+    @Column(name = "passcode")
+    @Size(min = 4,max = 5)
+    private String password;
+
+    public SignUpModel() {
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
