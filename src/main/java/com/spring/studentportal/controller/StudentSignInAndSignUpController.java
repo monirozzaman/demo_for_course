984e461e7e3f8167e6d43c3d07c39bc12120a26e
URP RAJUK <moniruzzaman35-223@diu.edu.bd>
2020-02-02 19:14:43 +0600

    Add Services

--- src/main/java/com/spring/studentportal/controller/StudentSignInAndSignUpController.java
+++ src/main/java/com/spring/studentportal/controller/StudentSignInAndSignUpController.java
@@ -1,27 +1,24 @@
 package com.spring.studentportal.controller;
 
 import com.spring.studentportal.dto.request.SignUpRequest;
-import com.spring.studentportal.model.SignUpModel;
-import com.spring.studentportal.repository.StudentRepository;
+import com.spring.studentportal.services.SingInAndSignUpService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/student/")
 public class StudentSignInAndSignUpController {
-    private final StudentRepository studentRepository;
+   private final SingInAndSignUpService singInAndSignUpService;
 
-    public StudentSignInAndSignUpController(StudentRepository studentRepository) {
-        this.studentRepository = studentRepository;
+    public StudentSignInAndSignUpController(SingInAndSignUpService singInAndSignUpService) {
+        this.singInAndSignUpService = singInAndSignUpService;
     }
 
     @PostMapping("signup")
-    public void signUp(@RequestBody SignUpRequest signUpRequest) {
-        SignUpModel signUpModel = new SignUpModel();
-        signUpModel.setStudentId(signUpRequest.getStudentId());
-        signUpModel.setName(signUpRequest.getStudentName());
-        signUpModel.setDept(signUpRequest.getStudentDept());
-        signUpModel.setPassword(signUpRequest.getStudentPass());
-        studentRepository.save(signUpModel);
+    public ResponseEntity<String> signUp(@RequestBody SignUpRequest signUpRequest) {
+       return new ResponseEntity(singInAndSignUpService.signUp(signUpRequest),
+               HttpStatus.CREATED);
     }
 
     @GetMapping("signIn")
